b4e0bb8101e12d4af71f7219485bd9eca0ebddb8
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-07-17 19:09:41 -0400

    first commit

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Meta from './Meta';
+
+export default class Page extends React.Component {
+    render() {
+        return (
+            <div>
+                <Meta />
+                <Header />
+                {this.props.children}
+            </div>
+        )
+    }
+}
